aea4f031dda4a94c467f4479d01e90f083b365ff
aferferc <aferferc@g.educaand.es>
2025-05-06 17:06:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask
 from routes.jugadores import jugadores
 from routes.usuarios import usuarios
-from flask_sqlalchemy import SQLAlchemy
 from utils.db import db
-import pymysql
-
-pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@localhost/jugadoresdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jugadores.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
